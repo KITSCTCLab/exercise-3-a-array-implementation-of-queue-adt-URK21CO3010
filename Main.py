e60595724ea16fcc9189193b1259d676bc3a5c52
URK21CO3010 <110015139+URK21CO3010@users.noreply.github.com>
2022-08-02 20:21:29 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -8,22 +8,18 @@ class Solution():
         self.stack = []
     
     def pop(self):
-        if not self.is_stack_empty():
             x = self.stack.pop()
             return x
     
     def push(self, item):
-        if not self.is_stack_full():
             self.stack.append(item)
     
     def dequeue(self):
-        if not self.is_queue_empty():
             self.front += 1
             x = self.queue[self.front]
             return x
     
     def enqueue(self, item):
-        if not self.is_queue_full():
             self.rear += 1
             self.queue.append(item)
 
